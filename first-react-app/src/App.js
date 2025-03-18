fba956a575b62d3bfd23d4d085acf376b53eea05
iamprashu <iamprashu.gs@gmail.com>
2025-03-18 08:57:04 +0530

    App and Css Files

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,19 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 import Items from './components/Items';
+import MainCard from './components/MainCard'
+import './output.css'
 
 
 function App() {
- 
+  const response = [
+    { name: "Nirma", day: 5, month: "March", year: 2023 },
+    { name: "Surf", day: 15, month: "July", year: 2021 },
+    { name: "Tide", day: 25, month: "December", year: 2022 },
+    { name: "Ariel", day: 10, month: "June", year: 2020 }
+  ];
+  
+ return (
+      <div className="bg-red-500 h-screen flex items-center justify-center">
+        <MainCard Data={response}></MainCard>
+        
 
-
-    
-  return (<div>
-    <Navigation></Navigation>
-    <Items name="Fruity" price = "12"></Items>
-    
-    
-    </div>
+      </div>
   );
 }
 
