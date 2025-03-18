8161a26fa86b310a212d40bce0a8edebdb706ea8
iamprashu <iamprashu.gs@gmail.com>
2025-03-18 08:57:37 +0530

    Items Js File Updated

--- first-react-app/src/components/Items.js
+++ first-react-app/src/components/Items.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
+import DateCard from "./DateCard";
 function Items(props){
-    
+    // const [name,ChangeIt] = useState(props.Obj.name)
+    // function ChangeName(){
+    //     ChangeIt("Namessddd")
+    // }
     return(
-        <div>
-            <h1>{props.name}</h1>
-            <h1>{props.price}</h1>
-            <h1>Item Name</h1>
+        <div className="bg-[#1b1b1b] h-[150px] w-[400px] rounded-xl p-5 text-white flex items-center justify-between">
+           
+            {/* <button onClick = {ChangeName}>Hello</button> */}
+
+            <DateCard Day = {props.Obj.day} Month = {props.Obj.month} Year = {props.Obj.year}></DateCard>
+            <p className="text-4xl">{props.Obj.name}</p>
 
         </div>
     )
 }
+
 
 export default Items;
